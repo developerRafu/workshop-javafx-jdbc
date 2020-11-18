2e1e4618c4f1fa404595b08a34084398d98a95ea
Rafael Henrique <fel.rique71@gmail.com>
2020-11-18 18:51:40 -0300

    SellerList

---
+++ src/model/services/SellerService.java
@@ -0,0 +1,35 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+/**
+ *
+ * @author Rafu
+ */
+public class SellerService {
+    private SellerDao dao = DaoFactory.createSellerDao();
+    
+    
+    public List<Seller> findAll(){
+        return dao.findAll();
+    }
+    
+    public void saveOrUpdate(Seller obj){
+        if(obj.getId()==null){
+            dao.insert(obj);
+        }else {
+            dao.update(obj);
+        }
+    }
+    
+    public void remove(Seller obj){
+        dao.deleteById(obj.getId());
+    }
+    
+}
