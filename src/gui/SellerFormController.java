451133528d0d55030844d22487a77b0f87d639bd
Rafael Henrique <fel.rique71@gmail.com>
2020-11-18 19:50:45 -0300

    TextField and DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -11,6 +11,9 @@ import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -22,8 +25,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.util.converter.LocalDateTimeStringConverter;
 import javax.xml.bind.ValidationException;
 import model.entities.Department;
 import model.entities.Seller;
@@ -47,7 +52,21 @@ public class SellerFormController implements Initializable{
     @FXML
     private TextField txtId;
     @FXML
+    private TextField txtEmail;
+    @FXML
+    private DatePicker dpBirthDate;
+    @FXML
+    private TextField txtBaseSalary;
+    @FXML
     private Label labelErrorName;
+    @FXML
+    private Label labelErrorEmail;
+    @FXML
+    private Label labelErrorBirthDate;
+    @FXML
+    private Label labelErrorBaseSalary;
+    
+    
     
     @FXML
     private Button btSave;
@@ -92,7 +111,11 @@ public class SellerFormController implements Initializable{
     
     private void initiliazeNodes(){
         Constraints.setTextFieldInteger(txtId);
-        Constraints.setTextFieldMaxLength(txtName, 30);
+        Constraints.setTextFieldMaxLength(txtName, 70);
+        Constraints.setTextFieldDouble(txtBaseSalary);
+        Constraints.setTextFieldMaxLength(txtEmail, 30);
+        Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+        
     }
 
     public void setSeller(Seller entity) {
@@ -109,7 +132,9 @@ public class SellerFormController implements Initializable{
         }
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
-        
+        txtEmail.setText(entity.getEmail());
+        txtBaseSalary.setText(""+entity.getBaseSalary());
+        //dpBirthDate.setValue(LocalDate.of(0, 0, 0));
     }
 
     private Seller getFormData() {
